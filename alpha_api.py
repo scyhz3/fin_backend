0dff48e2b568f8fbe8ea7a75e759c59106599aae
zhanghousu <1036746359@qq.com>
2025-09-10 20:47:46 +0900

    feat: add the first alpha api function

---
+++ alpha_api.py
@@ -0,0 +1,12 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.getenv('ALPHAVANTAGE_API_KEY')
+
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=' + api_key
+r = requests.get(url)
+data = r.json()
+
+print(data)
